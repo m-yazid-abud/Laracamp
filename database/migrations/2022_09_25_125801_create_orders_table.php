9c6245c2f229a2d0aa6197bd758413833b6c68dd
Muhammad Yazid <its.yazid.abud@gmail.com>
2022-09-25 20:26:54 +0700

    Create model, migration & running migration for Camp, Camp_benefit & Order

---
+++ database/migrations/2022_09_25_125801_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('camp_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->string("card_number", 20);
+            $table->string('cvc', 3);
+            $table->date('date');
+            $table->boolean("is_paid")->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
