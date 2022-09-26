72f8b861da129c83ebda0756eddcc463307c91c6
Muhammad Yazid <its.yazid.abud@gmail.com>
2022-09-26 07:02:07 +0700

    running useradminseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CampSeeder::class,
             CampBenefitSeeder::class,
+            UserAdminSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = [
+            [
+                'name' => "admin",
+                'email' => "admin@laracamp.develop",
+                'email_verified_at' => Date("Y-m-d H:i:s", time()),
+                'password' => Hash::make('password'),
+                'is_admin' => 1,
+            ],
+        ];
+
+        User::insert($admin);
+    }
+}
